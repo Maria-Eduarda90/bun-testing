ab20c7fc5e2e85efaa2ddbc8403507fd917494f7
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2023-10-27 14:18:35 -0300

    modified

--- src/core/user/service/repository/RepositoryUser.repository.ts
+++ src/core/user/service/repository/RepositoryUser.repository.ts
@@ -1,4 +1,4 @@
-import User from "../../model/Usuario.model";
+import User from "../../model/User.model";
 
 export default interface RepositoryUser {
     consultByEmail(email: string): Promise<User | null>;
