8e4a141b8c3c0045b7fd5013e27bec2b5bc937eb
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2023-10-28 00:27:32 -0300

    instantiating my classes in the index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import { Elysia } from "elysia";
+import RepositoryUserMemory from "./external/memory/RepositoryUser.memory";
+import RegisterUser from "./core/user/service/RegisterUser.service";
+import RegisterUserController from "./adapters/controller/RegisterUser.controller";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia();
+
+const repositoryUser = new RepositoryUserMemory();
+const regiterUser = new RegisterUser(repositoryUser);
+new RegisterUserController(app, regiterUser);
+
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
